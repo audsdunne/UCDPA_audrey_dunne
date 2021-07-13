74a5ac8634cccd811d83e3b36fb6e2f918af3211
audsdunne <audsdunne@yahoo.co.uk>
2021-07-13 11:59:40 +0100

    merging datasets on main file

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
 # Import external libraries required
-import statistics
-
 import pandas as pd
 import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statistics
 
 # Import data files
-apple_stock = pd.read_csv("AAPL_Montly_updates.csv")
+apple_stock_monthly = pd.read_csv("AAPL_Montly_updates.csv")
+apple_stock_weekly = pd.read_csv("AAPL_weekly_update.csv")
+apple_stock_daily = pd.read_csv("AAPL_daily_update.csv")
+
+# Combine 3 apple stock datasets and merge to one dataset
+apple_stock = pd.concat([apple_stock_daily, apple_stock_weekly, apple_stock_monthly], axis = 0)
 
 # Check for missing or N/A values
 print(apple_stock.isnull())
 print(apple_stock.isnull().sum())
 
 
-# Gain valuable insights
 
-# Identify Minimum Closing Stock Value
-closing_price = (apple_stock["Adj Close"])
-print(closing_price)
 
-min_closing_price = min(closing_price)
-print(min_closing_price)
+# Stop
 
-# Calculate the average closing stock value
-average_closing_price = statistics.mean(closing_price)
-print(average_closing_price)
+print(apple_stock.shape)
+print(apple_stock.info())
+print(apple_stock.describe().transpose)
+print(apple_stock.head(10))
+print(apple_stock.tail(10))
+
+# Check for missing or N/A values
+print(apple_stock.isnull())
+print(apple_stock.isnull().sum())
--- test.py
+++ test.py
@@ -4,9 +4,12 @@ import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 import seaborn as sns
+import statistics
 
 # Import data files
-apple_stock = pd.read_csv("AAPL_Montly_updates.csv")
+apple_stock_monthly = pd.read_csv("AAPL_Montly_updates.csv")
+apple_stock_weekly = pd.read_csv("AAPL_weekly_update.csv")
+apple_stock_daily = pd.read_csv("AAPL_daily_update.csv")
 
 # Understand different attributes of the dataset
 print(apple_stock.shape)
@@ -15,8 +18,23 @@ print(apple_stock.describe().transpose)
 print(apple_stock.head(10))
 print(apple_stock.tail(10))
 
+# Check for missing or N/A values
 print(apple_stock.isnull())
 print(apple_stock.isnull().sum())
+
+
+# Gain valuable insights
+
+# Identify Minimum Closing Stock Value
+closing_price = (apple_stock["Adj Close"])
+print(closing_price)
+
+min_closing_price = min(closing_price)
+print(min_closing_price)
+
+# Calculate the average closing stock value
+average_closing_price = statistics.mean(closing_price)
+print(average_closing_price)
 
 # Gain valuable insights
 
@@ -36,3 +54,16 @@ print(min_index)
 months = (apple_stock["Date"])
 min_month = months[min_index]
 print(min_month)
+
+# Gain valuable insights
+
+# Identify Minimum Closing Stock Value
+closing_price = (apple_stock["Adj Close"])
+print(closing_price)
+
+min_closing_price = min(closing_price)
+print(min_closing_price)
+
+# Calculate the average closing stock value
+average_closing_price = statistics.mean(closing_price)
+print(average_closing_price)
